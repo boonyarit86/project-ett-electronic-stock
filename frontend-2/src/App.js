5b55546a02e5740774a85e17cb42dcea8e31ca84
Boonyarit Ritdet <boonyaritza93@hotmail.com>
2022-05-05 17:46:08 +0700

    Update InputValidator component

--- frontend-2/src/App.js
+++ frontend-2/src/App.js
@@ -1,9 +1,28 @@
-import './App.css';
-import Heading from './Components/Text/Heading';
-import Title from './Components/Text/Title';
-import Test from './Test';
+import "./App.css";
+import Input from "./Components/InputWithValidator/InputWithValidator";
+import Heading from "./Components/Text/Heading";
+import Title from "./Components/Text/Title";
+import Test from "./Test";
+import { useForm } from "./hooks/form-hook";
+import { VALIDATOR_REQUIRE } from "./utils/validators";
 
 function App() {
+  const [formState, inputHandler] = useForm(
+    {
+      name: {
+        value: "",
+        isValid: false,
+      },
+    },
+    {
+      type: {
+        value: false,
+        isValid: false,
+      },
+    },
+    false
+  );
+
   return (
     <div className="App">
       <p>Hi</p>
@@ -12,6 +31,20 @@ function App() {
         <Heading type="main" text="Heading 1" />
         <Heading type="sub" text="Heading 2" />
         <Title>Hello Dev!!!!</Title>
+        <h3>Input</h3>
+        <Input
+          element="input"
+          type="email"
+          label="อีเมล์"
+          name="email"
+          placeholder="กรุณากรอกอีเมล์ของคุณ"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="โปรดใส่ข้อมูล."
+          onInput={inputHandler}
+          errorMessage="กรุณากรอกอีเมล์ของคุณ"
+          required
+          fullWidth
+        />
       </Test>
     </div>
   );
---
+++ frontend-2/src/Components/InputWithValidator/InputWithValidator.js
@@ -0,0 +1,121 @@
+import React, { useReducer, useEffect } from "react";
+import { validate } from "../../utils/validators";
+import "./InputWithValidator.css";
+
+// Input of Login page
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE":
+      return {
+        ...state,
+        value: action.val,
+        isValid: validate(action.val, action.validators),
+      };
+    case "TOUCH": {
+      return {
+        ...state,
+        isTouched: true,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const isInputEmpty = (isValid, isTouched, errorMessage) => {
+  return Boolean(!isValid && isTouched && errorMessage);
+};
+
+// Function Input ตัวจับ Error
+const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: props.initialValue || "",
+    isTouched: false,
+    isValid: props.initialValid || false,
+  });
+
+  const { id, onInput } = props;
+  const { value, isValid, isTouched } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
+
+  const changeHandler = (event) => {
+    dispatch({
+      type: "CHANGE",
+      val: event.target.value,
+      validators: props.validators,
+    });
+  };
+
+  const touchHandler = () => {
+    dispatch({
+      type: "TOUCH",
+    });
+  };
+
+  const element =
+    props.element === "input" ? (
+      // <TextField
+      //   error={!inputState.isValid && inputState.isTouched}
+      //   id={props.id}
+      //   label={
+      //     <div>
+      //       {props.label}{" "}
+      //       <span style={{ color: "red", fontWeight: "bold" }}>*</span>
+      //     </div>
+      //   }
+      //   variant="outlined"
+      //   fullWidth
+      //   type={props.type}
+      //   helperText={
+      //     !inputState.isValid && inputState.isTouched && props.errorText
+      //   }
+      //   onChange={changeHandler}
+      //   onBlur={touchHandler}
+      //   value={inputState.value}
+      //   style={{ margin: "20px 0" }}
+      //   InputLabelProps={{
+      //     shrink: props.shrink,
+      //   }}
+      //   disabled={props.disabled}
+      // />
+      <div
+        className={`input__box ${props.fullWidth && "fullWidth"} ${
+          isInputEmpty(isValid, isTouched, props.errorText) &&
+          "input__box-error"
+        }`}
+      >
+        <label className="input__label" htmlFor={props.name}>
+          {props.label} {props.required && <span className="input__required">*</span>}
+        </label>
+        <input
+          id={props.name}
+          type={props.type}
+          name={props.name}
+          placeholder={props.placeholder}
+          required={props.required}
+          onBlur={touchHandler}
+          onChange={changeHandler}
+        />
+        {isInputEmpty(isValid, isTouched, props.errorText) ? (
+          <span className="input__error-message">{props.errorMessage}</span>
+        ) : props.helperText ? (
+          <span>{props.helperText}</span>
+        ) : null}
+      </div>
+    ) : (
+      <textarea
+        id={props.id}
+        rows={props.rows || 3}
+        onChange={changeHandler}
+        onBlur={touchHandler}
+        value={inputState.value}
+      />
+    );
+
+  return <React.Fragment>{element}</React.Fragment>;
+};
+
+export default Input;
